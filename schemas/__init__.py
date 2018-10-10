0cc023e84b90ef626b27bcf841ed2bcd2feba79f
Ronnel Marfil <ronnel@sym.ph>
2018-10-11 04:05:11 +0800

    Add users query and register mutation

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -7,8 +7,8 @@ class Query(user.Query, graphene.ObjectType):
     node = graphene.relay.Node.Field()
 
 
-# class Mutation(user.Mutation, graphene.ObjectType):
-#     pass
+class Mutation(user.Mutation, graphene.ObjectType):
+    pass
 
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- schemas/user.py
+++ schemas/user.py
@@ -4,40 +4,35 @@ from graphene_gae import NdbObjectType, NdbConnectionField
 from models.user import User
 
 
-# class UserType(NdbObjectType):
-#     class Meta:
-#         model = User
-#         interfaces = (graphene.relay.Node,)
+class UserType(NdbObjectType):
+    class Meta:
+        model = User
+        interfaces = (graphene.relay.Node,)
 
 
 class Query(graphene.ObjectType):
-    # users = NdbConnectionField(UserType)
-    #
-    # def resolve_users(self, info, **kwargs):
-    #     return User.query()
+    users = NdbConnectionField(UserType)
 
-    x = graphene.String()
-
-    def resolve_x(self, info, **kwargs):
-        return "Hello"
+    def resolve_users(self, info, **kwargs):
+        return User.query()
 
 
-# class Register(graphene.relay.ClientIDMutation):
-#     class Input:
-#         name = graphene.String()
-#         email = graphene.String()
-#
-#     user = graphene.Field(UserType)
-#
-#     @classmethod
-#     def mutate_and_get_payload(cls, root, info, name, email):
-#         user = User()
-#         user.name = name
-#         user.email = email
-#         user.put()
-#
-#         return Register(user=user)
-#
-#
-# class Mutation(graphene.ObjectType):
-#     register = Register.Field(description="Register a new user")
+class Register(graphene.relay.ClientIDMutation):
+    class Input:
+        name = graphene.String()
+        email = graphene.String()
+
+    user = graphene.Field(UserType)
+
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, name, email):
+        user = User()
+        user.name = name
+        user.email = email
+        user.put()
+
+        return Register(user=user)
+
+
+class Mutation(graphene.ObjectType):
+    register = Register.Field(description="Register a new user")
